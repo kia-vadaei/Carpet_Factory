f0ac4803ce2df38741b6037e6969a02a56cbd253
melika shirian <106620989+emika12@users.noreply.github.com>
2023-05-11 19:11:19 +0430

    finding the least color we need to color a graph algorithm !

---
+++ carpetFact/src/LeastColor.py
@@ -0,0 +1,65 @@
+
+
+
+
+class FindLeastColor :
+
+    graph = None
+    vertex = int
+
+
+    def getting_carpet_adjacency_matrix(self):
+        print("please enter carpet matrix : first enter the amount of areas:")
+
+        area = input()
+
+        carpet_matrix = [[0] * area for x in range (area)]
+
+
+        print("now please enter the adjacency of each of the areas : ")
+
+        for row in carpet_matrix:
+            tmp_row = input().split(' ')
+            for i in range(len(row)):
+                row[i] = int(tmp_row[i])
+
+        self.graph = carpet_matrix
+        self.vertex = area
+
+        color  =  area
+        self.graphColouring(color)
+
+
+
+    # is safe for vertex v
+    def isSafe(self, v, colour, c):
+        for i in range(self.vertex):
+            if self.graph[v][i] == 1 and colour[i] == c:
+                return False
+        return True
+
+    # A recursive utility function to solve m
+    # coloring  problem
+    def graphColourUtil(self, m, colour, v):
+        if v == self.vertex:
+            return True
+
+        for c in range(1, m + 1):
+            if self.isSafe(v, colour, c) == True:
+                colour[v] = c
+                if self.graphColourUtil(m, colour, v + 1) == True:
+                    return True
+                colour[v] = 0
+
+    def graphColouring(self, m):
+        colour = [0] * self.vertex
+        if self.graphColourUtil(m, colour, 0) == False:
+            return False
+
+        # Print the solution
+        print
+        "Solution exist and Following are the assigned colours:"
+        for c in colour:
+            print
+            c,
+        return True
