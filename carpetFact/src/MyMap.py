4a5371e9ee9cd56125a1688b90314b47606def2f
melika shirian <106620989+emika12@users.noreply.github.com>
2023-05-11 23:06:12 +0430

    merging sort and similarities 2! adding map for sorting

---
+++ carpetFact/src/MyMap.py
@@ -0,0 +1,12 @@
+from Carpet import Carpet
+
+
+class MyMap:
+    carpet_matrix = Carpet
+    value = int
+
+    def __init__(self , carpet_matrix , value):
+        self.carpet_matrix = carpet_matrix
+        self.value = value
+
+
